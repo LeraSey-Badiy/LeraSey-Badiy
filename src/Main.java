3019d50645cfda006700a88c5cc70e745535ad2c
LeraSey-Badiy <tapochek373@gmail.com>
2024-03-28 11:48:57 +0300

    malina2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        // IntelliJ IDEA suggests fixing it.hh
         System.out.printf("Hel7lobb an79d7 welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
